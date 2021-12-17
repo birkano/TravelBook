797e1b47e98f071a9d68dadb3d7698cc7ace2174
Birkan <birkanpusa@icloud.com>
2021-12-17 20:31:16 +0300

    no message

--- TravelBook/ViewController.swift
+++ TravelBook/ViewController.swift
@@ -9,12 +9,19 @@ import UIKit
 import MapKit
 //konum alabilmek için corelocation
 import CoreLocation
+//veritabanı için
+import CoreData
 
 //Sınıfları viewcontroller'e tanımlıyoruz
 class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var commentText: UITextField!
+    //değişkenler tanımlandı
     var locationManager = CLLocationManager()
+    var chosenLatitude = Double()
+    var chosenLongtitude = Double()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +34,51 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         locationManager.requestWhenInUseAuthorization()
         //konumu güncellemeye başladık
         locationManager.startUpdatingLocation()
+        
+        
+        //klavye kapama
+        let hideKeyboardRecognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        mapView.addGestureRecognizer(hideKeyboardRecognizer)
+
+        //uzun dokunulduğu zaman devreye girer
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(chooseLocation(gestureRecognizer:)))
+        // basılı tutma süresi
+        gestureRecognizer.minimumPressDuration = 2
+        mapView.addGestureRecognizer(gestureRecognizer)
+        
+    }
+    
+    //klavye kapama fonksiyonu
+    @objc func hideKeyboard() {
+        mapView.endEditing(true)
+    }
+    
+    //Uzun dokunulduğu zaman ne olacak
+    @objc func chooseLocation(gestureRecognizer:UILongPressGestureRecognizer) {
+        
+        if gestureRecognizer.state == .began {
+            //Nereye dokunulduğunu al
+            let touchedPoint = gestureRecognizer.location(in: self.mapView)
+            //dokunulan yeri koordinata çevir
+            let touchedCoordinates = self.mapView.convert(touchedPoint, toCoordinateFrom: self.mapView)
+            
+            
+            //değişkenlere koordinatları gönderdik
+            chosenLatitude = touchedCoordinates.latitude
+            chosenLongtitude = touchedCoordinates.longitude
+            
+            //pin oluştur
+            let annotation = MKPointAnnotation()
+            //dokunulan coordinati al
+            annotation.coordinate = touchedCoordinates
+            //Title
+            annotation.title = nameText.text
+            //subtitle
+            annotation.subtitle = commentText.text
+            //ekliyoruz
+            self.mapView.addAnnotation(annotation)
+        }
+        
     }
     
     //konumu mapte göstermek için gerekli fonksiyon
@@ -40,7 +92,32 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         
         
     }
-
+    
+    
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        
+        let appDeletagate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDeletagate.persistentContainer.viewContext
+        
+        let newPlace = NSEntityDescription.insertNewObject(forEntityName: "Places", into: context)
+        
+        newPlace.setValue(nameText.text, forKey: "title")
+        newPlace.setValue(commentText.text, forKey: "subtitle")
+        newPlace.setValue(chosenLatitude, forKey: "latitude")
+        newPlace.setValue(chosenLongtitude, forKey: "longtitude")
+        newPlace.setValue(UUID(), forKey: "id")
+        
+        do{
+            try context.save()
+            print("success")
+            
+        } catch {
+            print("failed")
+        }
+        
+    }
+    
+    
 
 }
 
