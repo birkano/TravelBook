2dd08fbe27db2fc1f4c662b002a441e9552a7f6f
Birkan <birkanpusa@icloud.com>
2021-12-17 19:38:58 +0300

    lokasyon izni, konum güncelleme, konumu mapte göstermek için gerekli fonksiyon

--- TravelBook/ViewController.swift
+++ TravelBook/ViewController.swift
@@ -6,12 +6,39 @@
 //
 
 import UIKit
+import MapKit
+//konum alabilmek için corelocation
+import CoreLocation
 
-class ViewController: UIViewController {
-
+//Sınıfları viewcontroller'e tanımlıyoruz
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        mapView.delegate = self
+        locationManager.delegate = self
+        // en iyi konumu al dedik
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        //izin istedik
+        locationManager.requestWhenInUseAuthorization()
+        //konumu güncellemeye başladık
+        locationManager.startUpdatingLocation()
+    }
+    
+    //konumu mapte göstermek için gerekli fonksiyon
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //koordinatı diziden aldık
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        //zoom seviyesi ayarlama
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        
     }
 
 
